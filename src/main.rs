fb1f32514782102e8fdc33a7c4f5e55da215f8e3
Ward Gauderis <ward.gauderis@student.uantwerpen.be>
2021-07-18 01:33:05 +0200

    up to hardware interrupts

---
+++ src/main.rs
@@ -0,0 +1,43 @@
+#![no_std]
+#![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(os::test_runner)]
+#![reexport_test_harness_main = "test_main"]
+
+use core::panic::PanicInfo;
+use os::println;
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+	println!("Hello World{}", "!");
+
+	os::init();
+
+	fn overflow(){
+		overflow();
+	}
+
+	overflow();
+
+	#[cfg(test)]
+	test_main();
+
+	println!("It did not crash!");
+	loop {}
+}
+
+#[cfg(not(test))]
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+	println!("{}", _info);
+	loop {}
+}
+
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! { os::test_panic_handler(info) }
+
+#[test_case]
+fn trivial_assertion() {
+	assert_eq!(1, 1);
+}
